c624764184df12b86d34d8e2c77e29e91d8e7f5d
kellydinneen <kgdinneen@gmail.com>
2021-01-14 13:35:11 -0700

    Draft properties of travelers and trips

---
+++ src/traveler.js
@@ -0,0 +1,6 @@
+class Traveler {
+  constructor(name, tripBook) {
+    this.name = name;
+    this.tripBook = tripBook || [];
+  }
+}
---
+++ src/trip.js
@@ -0,0 +1,9 @@
+class Trip {
+  constructor(destination, departureDate, duration, numberOfTravelers) {
+    this.destination = destination;
+    this.departureDate = departureDate;
+    this.duration = duration;
+    this.numberOfTravelers = numberOfTravelers;
+    this.status = "pending";
+  }
+}
