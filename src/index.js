3c5101054caaf4724312a79b6ab011bf906d472e
kellydinneen <kgdinneen@gmail.com>
2021-01-14 15:44:16 -0700

    build out minimum dashboard styling

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 // Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/base.scss';
-// import './css/index.scss';
+// import './css/base.scss';
+import './css/index.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
