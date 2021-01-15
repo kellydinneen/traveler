cfdcff4062d14a5ce33d0c05f63be667b4c3b720
kellydinneen <kgdinneen@gmail.com>
2021-01-14 17:58:37 -0700

    Calculate cost of years trips

--- src/traveler.js
+++ src/traveler.js
@@ -1,15 +1,20 @@
 class Traveler {
-  constructor(id, name, travelerType, tripBook) {
+  constructor(allTripData, allDestinationData) {
     this.id = id;
     this.name = name;
     this.travelerType = travelerType;
-    this.tripBook = tripBook || [];
+    this.tripBook = allTripData.reduce((allUserTrips, trip) => {
+      if(trip.userID === this.userID){
+        let newTrip = new Trip(trip, this.destinations);
+        allUserTrips.push(newTrip);
+      }
+      return acc;
+    }, []);
   }
 
   calculateAnnualTravelSpending() {
-    (acc, trip) => {
-      
-    }
-    this.tripBook.reduce(,0)
+    return this.tripBook.reduce((totalCost, trip) => {
+      return totalCost += trip.calculateTripCost();
+    },0)
   }
 }
