ee84313fe20a7f24ece16e8d087336ce1df140c5
kellydinneen <kgdinneen@gmail.com>
2021-01-14 18:20:28 -0700

    Display all trip categories on one page

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -16,4 +16,22 @@ let domUpdates = {
   displayOneLiners(element, text) {
   element.innerText = text;
   },
+
+  interactWithClassList(method, className, event, element) {
+    if (method === 'add' && element && className) {
+      element.classList.add(className);
+    } else if (method === 'remove' && element && className) {
+      element.classList.remove(className);
+    } else if (method === 'contains' && className && element) {
+      return element.classList.contains(className);
+    } else if (method === 'add' && element === undefined && className) {
+      event.target.classList.add(className);
+    } else if (method === 'remove' && element === undefined && className) {
+      event.target.classList.remove(className);
+    } else if (method === 'contains' && className && element === undefined) {
+      return event.target.classList.contains(className);
+    } else if (method === 'toggle' && element && className) {
+      element.classList.toggle(className);
+    }
+  },
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@
 import './css/index.scss';
 import Traveler from './traveler';
 import Trip from './trip';
+import domUpdates from './domUpdates';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+let myTripsButton = document.querySelector('.my-trips-button');
+let browseDestinationsButton = document.querySelector('.browse-destinations-button');
+let myTripsPage = document.querySelector('.my-trips');
